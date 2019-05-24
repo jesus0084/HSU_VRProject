19c532bbd1260ca3f413ce7a46961b1400714698
jesus0084 <jesus0084@naver.com>
2019-05-24 16:10:11 +0900

    조이스틱-마우스 동시 대응

--- MyGame/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ MyGame/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -212,9 +212,11 @@ namespace UnityStandardAssets.Characters.FirstPerson
 #if !MOBILE_INPUT
             // On standalone builds, walk/run speed is modified by a key press.
             // keep track of whether or not the character is walking or running
+            
+            /*original code
             m_IsWalking = !Input.GetKey(KeyCode.LeftShift);
-
-            //m_IsWalking = !Input.GetButton("Fire3");
+            */
+            m_IsWalking = !Input.GetButton("Run");
 #endif
             // set the desired speed to be walking or running
             speed = m_IsWalking ? m_WalkSpeed : m_RunSpeed;
